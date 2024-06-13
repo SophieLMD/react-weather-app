050ea080176198199c51cd9c0b2d8abe7cdb4f03
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-06-13 06:11:48 +0100

    Updated footer links in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import "./App.css";
 
 function App() {
   return <div className="App">Weather Application</div>;
+  <footer>
+    Coded by {}
+    <a href="https://github.com/SophieLMD" target="blank">
+      Sophie Davies
+    </a>
+    , open sourced on {}
+    <a href="https://github.com/SophieLMD/react-weather-app" target="blank">
+      Github {}
+    </a>
+    and website hosted on {}
+    <a href="https://react-weather-app-slmd.netlify.app/" target="blank">
+      Netlify
+    </a>
+  </footer>;
 }
 
 export default App;
