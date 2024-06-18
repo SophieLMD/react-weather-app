58c75d287ecbe806452fa27523f1a47dde9de1b4
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-06-18 21:06:45 +0100

    Adjusted styling of weather.js component

--- src/Weather.js
+++ src/Weather.js
@@ -10,6 +10,7 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-entry"
+              autoFocus="on"
             />
           </div>
           <div class="col-3">
@@ -22,27 +23,35 @@ export default function Weather() {
         </div>
       </form>
       <h1>Cardiff</h1>
-      <div className="row">
-        <div className="col-3">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
-            alt="rain"
-          />{" "}
-          9°C
+      <div className="row mt-3 mb-4">
+        <div className="col-4">
+          <div className="clearfix d-flex justify-content-start">
+            <img
+              className="float-left"
+              src="https://ssl.gstatic.com/onebox/weather/64/rain.png"
+              alt="rain"
+            />{" "}
+            <div className="float-left">
+              <span className="temperature">9</span>
+              <span className="unit">°C | F</span>
+            </div>
+          </div>
         </div>
-        <div className="col-3">
+        <div className="col-4">
           <ul>
             <li>Precipitation: 92%</li>
             <li>Humidity: 92%</li>
             <li>Wind: 5mph</li>
           </ul>
         </div>
-        <div className="col-6">
-          <h2>Weather</h2>
-          <ul>
-            <li>Friday 20:00</li>
-            <li>Rain</li>
-          </ul>
+        <div className="col-4 position-relative">
+          <div className="position-absolute top-0 start-50 text-end">
+            <h2>Weather</h2>
+            <ul>
+              <li className="current-conditions">Friday 20:00</li>
+              <li className="current-conditions">Rain</li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className="weather-links">
