f4fc9865dd32358760858fe073013cb1643aad45
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-06-23 18:52:38 +0100

    Replaced iconUrl with the correct icon url

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/rain.png",
+      iconUrl: `https://openweathermap.org/img/wn/{response.data.weather[0]icon}@2x.png`,
     });
   }
   function handleSubmit(event) {
