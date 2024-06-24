7edefd56666e5521bbb37fd1bc0f7769f2878096
SophieLMD <160946529+SophieLMD@users.noreply.github.com>
2024-06-24 05:33:02 +0100

    Changed apiUrl from http to https to address bug when deploying

--- src/Weather.js
+++ src/Weather.js
@@ -26,7 +26,7 @@ export default function Weather(props) {
   }
   function search() {
     const apiKey = "866a208a73eeff02182218e9441647a1";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
   if (weatherData.ready) {
